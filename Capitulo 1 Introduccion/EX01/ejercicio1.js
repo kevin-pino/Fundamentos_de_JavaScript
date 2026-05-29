bcdcfc1eed18e09456b167a5d40ab507033d2e3c
Kevin-Pino <kevinpinokf7@gmail.com>
2026-05-28 20:13:36 -0500

    Primeros pasos en js

---
+++ Capitulo 1 Introduccion/EX01/ejercicio1.js
@@ -0,0 +1,9 @@
+/*
+Esto sirve para realizar comentarios en el código, es decir, para escribir texto que no se ejecutará como código.
+Los comentarios son útiles para explicar el propósito de ciertas partes del código, para dejar notas para otros desarrolladores o para recordar algo importante sobre el código en el futuro.
+*/
+
+// Esto es un comentario de una sola línea, se utiliza para escribir comentarios breves o para comentar una sola línea de código. 
+
+document.write("Hola Mundo");
+console.log("Hola desde la consola");
